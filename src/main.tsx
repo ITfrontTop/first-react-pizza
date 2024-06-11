1866e11db1855cc4f9b596dea91948358a7d6a8e
ITfrontTop <Fine1337@ukr.net>
2024-06-11 22:55:53 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,91 @@
+import React, { Suspense, lazy } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom';
+// import { Menu } from './pages/Menu/Menu.tsx';
+import { Cart } from './pages/Cart/Cart.tsx';
+import { Error as ErrorPage } from './pages/Error/Error.tsx';
+import { Layout } from './layout/Menu/Layout.tsx';
+import { Product } from './pages/Product/Product.tsx';
+import { PREFIX } from './helpers/API.ts';
+import axios from 'axios';
+import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
+import { Login } from './pages/Login/Login.tsx';
+import { Register } from './pages/Registr/Registr.tsx';
+import { RequireAuth } from './helpers/RequireAuth.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
+
+const Menu = lazy(() => import('./pages/Menu/Menu'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <RequireAuth>
+        <Layout />
+      </RequireAuth>
+    ),
+    children: [
+      {
+        path: '/',
+        element: (
+          <Suspense fallback={<>Загрузка...</>}>
+            <Menu />
+          </Suspense>
+        )
+      },
+      {
+        path: '/cart',
+        element: <Cart />
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+        errorElement: <>Ошибка</>,
+        loader: async ({ params }) => {
+          return defer({
+            data: new Promise((resolve, reject) => {
+              setTimeout(() => {
+                axios
+                  .get(`${PREFIX}/products/${params.id}`)
+                  .then((data) => resolve(data))
+                  .catch((e) => reject(e));
+              }, 2000);
+            })
+          });
+        }
+      }
+    ]
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <ErrorPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
